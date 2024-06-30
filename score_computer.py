380cf450d018199143e0e5a22651d74d9d6431ec
mmm1379 <mmmasoudpour@gmail.com>
2024-06-30 13:47:40 +0330

    fix score_computer

--- score_computer.py
+++ score_computer.py
@@ -6,14 +6,13 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline
 
 
-def initialize_nlp():
-    global nlp
+def get_nlp():
     # Load the tokenizer and model
     model_name = "HooshvareLab/bert-fa-base-uncased-sentiment-snappfood"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     # Create a pipeline for sentiment analysis
-    nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    return pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 
 def get_sheet():
@@ -30,13 +29,13 @@ def calculate_final_score(row):
 
 
 if __name__ == '__main__':
-
-    initialize_nlp()
+    nlp = get_nlp()
     sheet = get_sheet()
     source_data = sheet.get_all_values()
+    sheet.update_cell(1, 10, "final score")
 
     for index, row in enumerate(source_data):
-        if index == 1 or row[8]:
+        if index == 0 or row[9]:
             continue
         score = calculate_final_score(row)
-        sheet.update_cell(index + 1, 8, score)
+        sheet.update_cell(index + 1, 10, score)
